e5a80c1b5de5176e8ae10e52ae0a15a58e095155
Heredin <herevel77@gmail.com>
2020-02-09 18:18:53 -0500

    Heredin
    Subiendo cambios para poder compartir audio y video en la app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,55 +9,135 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  RTCVideoRenderer _localRenderer=RTCVideoRenderer();
-   Signaling _signaling=Signaling();
+  String _me;
+  String _username = '';
+  RTCVideoRenderer _localRenderer = RTCVideoRenderer();
+  RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
+  Signaling _signaling = Signaling();
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _localRenderer.initialize();
+    _remoteRenderer.initialize();
+
     _signaling.init();
-    _signaling.onLocalStream=(MediaStream stream){
-      _localRenderer.srcObject=stream;
-      _localRenderer.mirror=true;
+    _signaling.onLocalStream = (MediaStream stream) {
+      _localRenderer.srcObject = stream;
+      _localRenderer.mirror = true;
+    };
+    _signaling.onRemoteStream = (MediaStream stream) {
+      _remoteRenderer.srcObject = stream;
+      //queremos visualizar los datos de video de la otra persona
+      _remoteRenderer.mirror=true;
+    };
+    _signaling.onJoined = (bool isOk) {
+      if (isOk) {
+        setState(() {
+          _me = _username;
+        });
+      }
     };
   }
+
   @override
   void dispose() {
     _signaling.dispose();
-    _localRenderer?.dispose();
+    _localRenderer.dispose();
+    _remoteRenderer.dispose();
     super.dispose();
   }
+
+  _inputCall() {
+    var username = '';
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            content: CupertinoTextField(
+              placeholder: "LLamando a",
+              onChanged: (text) => username = text,
+            ),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                onPressed: () {
+                  _signaling.call(username);
+                  Navigator.pop(context);
+                },
+                child: Text('Hablar'),
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              left: 20,
-             bottom: 40,
-             child: Transform.scale(
-                 scale: 0.3,
-               alignment: Alignment.bottomLeft,
-               child: ClipRRect(
-                 borderRadius:BorderRadius.circular(20.0),
-                 child: Container(
-                   width: 480,
-                   height: 640 ,
-                   color: Colors.black12,
-                   child: RTCVideoView(
-_localRenderer
-                   ),
-                 ),
-               ),),
-           )
-          ],
-        ),
+        child: _me == null
+            ? Padding(
+                padding: EdgeInsets.all(30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CupertinoTextField(
+                      placeholder: 'Tu Nombre ',
+                      textAlign: TextAlign.center,
+                      onChanged: (text) => _username = text,
+                    ),
+                    SizedBox(height: 20),
+                    CupertinoButton(
+                      child: Text('Unirse'),
+                      color: Colors.blue,
+                      onPressed: () {
+                        if (_username.trim().length == 0) {
+                          return;
+                        }
+                        _signaling.emit('join', _username);
+                      },
+                    )
+                  ],
+                ),
+              )
+            : Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: RTCVideoView(_remoteRenderer),
+                  ),
+                  Positioned(
+                    left: 20,
+                    bottom: 40,
+                    child: Transform.scale(
+                      scale: 0.3,
+                      alignment: Alignment.bottomLeft,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Container(
+                          width: 480,
+                          height: 640,
+                          color: Colors.black12,
+                          child: RTCVideoView(_localRenderer),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                      right: 20,
+                      bottom: 40,
+                      child: CupertinoButton(
+                        onPressed: _inputCall,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        color: Colors.green,
+                        child: Text("LLamar"),
+                      )
+                  )
+                ],
+              ),
       ),
     );
   }
 }
-
--- lib/utils/signaling.dart
+++ lib/utils/signaling.dart
@@ -2,36 +2,131 @@ import 'package:flutter_webrtc/webrtc.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 typedef OnLocalStream(MediaStream stream);
+typedef OnRemoteStream(MediaStream stream);
+typedef OnJoined(bool isOk);
 
-class Signaling{
+class Signaling {
   IO.Socket _socket;
   OnLocalStream onLocalStream;
-  init()async{
-   MediaStream stream= await navigator.getUserMedia(
-        {
-          "audio": true,
-          "video": {
-            "mandatory": {
-              "minWidth": '480',
-              // Provide your own width, height and frame rate here
-              "minHeight": '640',
-              "minFrameRate": '30',
-            },
-            "facingMode": "user",
-            "optional": [],
-          }
-        });
-     onLocalStream(stream);
+  OnRemoteStream onRemoteStream;
+  OnJoined onJoined;
+  RTCPeerConnection _peer;
+  String _him;
+  MediaStream _localStream;
+
+  init() async {
+    MediaStream stream = await navigator.getUserMedia({
+      "audio": true,
+      "video": {
+        "mandatory": {
+          "minWidth": '480',
+          // Provide your own width, height and frame rate here
+          "minHeight": '640',
+          "minFrameRate": '30',
+        },
+        "facingMode": "user",
+        "optional": [],
+      }
+    });
+    _localStream = stream;
+    onLocalStream(stream);
+    _connect();
   }
 
-  _connect(){
-    _socket = IO.io('https://backend-simple-webrtc.herokuapp.com', <String, dynamic>{
-      'transports': ['websocket'],
+  _createPeer() async {
+    this._peer = await createPeerConnection({
+      "iceServers": [
+        {
+          "urls": ['stun:stun1.l.google.com:19302']
+        }
+      ]
+    }, {});
+    await _peer.addStream(_localStream);
+    _peer.onIceCandidate = (RTCIceCandidate candidate) {
+      if (candidate == null) {
+        return;
+      }
+      print('send the IceCandidate');
+      emit('candidate', {"username":_him,"candidate":candidate.toMap()});
+    };
+    _peer.onAddStream = (MediaStream remoteStream) {
+      onRemoteStream(remoteStream);
+    };
+  }
 
+  _connect() {
+    _socket =
+        IO.io('https://backend-simple-webrtc.herokuapp.com', <String, dynamic>{
+      'transports': ['websocket'],
+    });
+    _socket.on('on-join', (isOk) {
+      print("Unido jejej $isOk");
+      onJoined(isOk);
+    });
+
+    _socket.on('on-call', (data) async {
+      print("En llamada jejej $data");
+      //hemos creado el peer para recibir la llamada del otro usuario
+      await _createPeer();
+      final String username = data['username'];
+      _him = username;
+      final offer = data['offer'];
+      final RTCSessionDescription desc = RTCSessionDescription(offer['sdp'], offer['type']);
+
+      await _peer.setRemoteDescription(desc);
+      //aqui espero recibir el audio y el video
+      final sdpConstraints = {
+        "mandatory": {
+          "OfferToReceiveAudio": true,
+          "OfferToReceiveVideo": true,
+        },
+        "optional": [],
+      };
+      //creando la respuesta del otro usuario
+      final RTCSessionDescription answer =
+          await _peer.createAnswer(sdpConstraints);
+      await _peer.setLocalDescription(answer);
+      emit('answer', {"username": _him, "answer": answer.toMap()});
+    });
+    //capturar la respuesta que me dio  el otro usuario
+    _socket.on('on-answer', (answer) {
+      print("on-answer $answer");
+      final RTCSessionDescription desc =
+          RTCSessionDescription(answer['sdp'], answer['type']);
+      _peer.setRemoteDescription(desc);
+    });
+
+    _socket.on('on-candidate', (data)async {
+      print("on-candidate $data");
+      final RTCIceCandidate candidate=RTCIceCandidate(data['candidate'],data['sdpMid'],data['sdpMLineIndex']);
+
+      await _peer.addCandidate(candidate);
     });
   }
 
-dispose(){
+  emit(String eventName, dynamic data) {
+    _socket?.emit(eventName, data);
+  }
+
+  call(String username) async {
+    _him=username;
+    await _createPeer();
+    //Aqui le digo a la persona que quiero obtener el audio y video
+    final sdpConstraints = {
+      "mandatory": {
+        "OfferToReceiveAudio": true,
+        "OfferToReceiveVideo": true,
+      },
+      "optional": [],
+    };
+    final RTCSessionDescription offer = await _peer.createOffer(sdpConstraints);
+    //registrar en el peer
+    _peer.setLocalDescription(offer);
+    //map me retorna el sdp y type
+    emit('call', {"username": username, "offer": offer.toMap()});
+  }
+
+  dispose() {
     _socket?.disconnect();
-}
+  }
 }
